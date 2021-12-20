dbd7595bf5ef11ee7768f3446ce7cce9d69f1b49
Bravo <djdram@ya.ru>
2021-12-21 02:12:04 +0300

    Update EthereumPoolPaymentProcessingConfigExtra.cs
    add params

--- src/Miningcore/Blockchain/Ethereum/Configuration/EthereumPoolPaymentProcessingConfigExtra.cs
+++ src/Miningcore/Blockchain/Ethereum/Configuration/EthereumPoolPaymentProcessingConfigExtra.cs
@@ -1,5 +1,5 @@
 namespace Miningcore.Blockchain.Ethereum.Configuration;
-
+{
 public class EthereumPoolPaymentProcessingConfigExtra
 {
     /// <summary>
@@ -11,14 +11,30 @@ public class EthereumPoolPaymentProcessingConfigExtra
     /// True to exempt uncle rewards from miner rewards
     /// </summary>
     public bool KeepUncles { get; set; }
-
+	
+    /// <summary>
+    /// True let geth to determine gas and gasPrice
+    /// </summary>
+    public bool? AutoGas { get; set; }
+	
     /// <summary>
     /// Gas amount for payout tx (advanced users only)
     /// </summary>
     public ulong Gas { get; set; }
 
     /// <summary>
+    /// Gas price for payout tx (advanced users only)
+    /// </summary>
+    public ulong GasPrice { get; set; }
+
+    /// <summary>
+    /// if true commission is charged from the miner payout amount is relevant for Ethereum EIP-1559 
+    /// </summary>
+    public bool? MinerTxFee { get; set; }
+
+    /// <summary>
     /// maximum amount you’re willing to pay
     /// </summary>
     public ulong MaxFeePerGas { get; set; }
+}
 }
