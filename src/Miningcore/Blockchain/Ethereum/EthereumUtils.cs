fd14127109d26e9e224084ef7b78b4632db270da
Bravo <djdram@ya.ru>
2021-12-20 15:31:48 +0300

    Update Ethereum Utils
    add chainType
    add GetTargetHex

--- src/Miningcore/Blockchain/Ethereum/EthereumUtils.cs
+++ src/Miningcore/Blockchain/Ethereum/EthereumUtils.cs
@@ -1,32 +1,49 @@
-namespace Miningcore.Blockchain.Ethereum;
+using System;
+using System.Linq;
+using System.Numerics;
+using Nethereum.Hex.HexConvertors.Extensions;
 
-public class EthereumUtils
+namespace Miningcore.Blockchain.Ethereum
 {
-    public static void DetectNetworkAndChain(string netVersionResponse, string gethChainResponse,
-        out EthereumNetworkType networkType, out GethChainType chainType)
+    public class EthereumUtils
     {
-        // convert network
-        if(int.TryParse(netVersionResponse, out var netWorkTypeInt))
+        public static void DetectNetworkAndChain(string netVersionResponse, string gethChainResponse,
+            out EthereumNetworkType networkType, out GethChainType chainType)
         {
-            networkType = (EthereumNetworkType) netWorkTypeInt;
+            // convert network
+            if(int.TryParse(netVersionResponse, out var netWorkTypeInt))
+            {
+                networkType = (EthereumNetworkType) netWorkTypeInt;
 
-            if(!Enum.IsDefined(typeof(EthereumNetworkType), networkType))
+                if(!Enum.IsDefined(typeof(EthereumNetworkType), networkType))
+                    networkType = EthereumNetworkType.Unknown;
+            }
+
+            else
                 networkType = EthereumNetworkType.Unknown;
+
+            // convert chain
+            if(!Enum.TryParse(gethChainResponse, true, out chainType))
+            {
+               chainType = GethChainType.Unknown;
+            }
+
+            if(chainType == GethChainType.Ethereum)
+                chainType = GethChainType.Ethereum;
+
+            if(chainType == GethChainType.Callisto)
+                chainType = GethChainType.Callisto;
+
+            if(chainType == GethChainType.Expanse)
+                chainType = GethChainType.Expanse;
+
         }
-
-        else
-            networkType = EthereumNetworkType.Unknown;
-
-        // convert chain
-        if(!Enum.TryParse(gethChainResponse, true, out chainType))
+        public static string GetTargetHex(double difficulty)
         {
-            chainType = GethChainType.Unknown;
+            var diff = new BigInteger(difficulty * EthereumConstants.Pow2x32);
+            var target = BigInteger.Divide(EthereumConstants.BigMaxValue, diff);
+            var hex = target.ToByteArray(true, true).ToHex();
+            return $"0x{string.Concat(Enumerable.Repeat("0", 64 - hex.Length))}{hex}";
         }
-
-        if(chainType == GethChainType.Ethereum)
-            chainType = GethChainType.Ethereum;
-
-        if(chainType == GethChainType.Callisto)
-            chainType = GethChainType.Callisto;
     }
 }
